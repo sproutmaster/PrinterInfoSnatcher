18661d4659ec2947aed5f2ad2cceaa7912e8cb60
Joe Prakash <joep@nyu.edu>
2023-05-12 15:57:40 -0400

    not working

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ app.use(helmet());
 app.use(morgan('tiny'));
 app.use(cors());
 
+// for storing references to objects
+let system = {
+    browser : null,
+    printer : null
+};
+
+
 let response_message = {
     info: {
         api_name: "Printer Info Snatcher",
@@ -34,110 +41,136 @@ let browser_params = {
     timeout: 5000
 };
 
-async function get_device_details(browser, printer_obj){
-    const page = await browser.newPage();
-    await page.goto(`https://${printer_obj.host}/hp/device/DeviceInformation/View`, browser_params);
-
-    printer_obj.model = await page.evaluate(()=> {
-        return document.querySelector("#ProductName").textContent;
-    })
-    printer_obj.name = await page.evaluate(()=> {
-        return document.querySelector("#DeviceName").textContent;
+async function open_browser(){
+    system.browser = await pupp.launch({
+        headless: true,
+        devtools: false,
+        ignoreHTTPSErrors: true,
+        args: ['--incognito', '--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-sandbox']
     });
-    printer_obj.serial = await page.evaluate(()=> {
-        return document.querySelector("#DeviceSerialNumber").textContent;
-    });
-    printer_obj.location = await page.evaluate(()=> {
-        return document.querySelector("#DeviceLocation").textContent;
-    });
-
-    return data;
 }
 
-async function get_supply_details(browser, printer_obj){
-    const page = await browser.newPage();
-    await page.goto(`https://${printer_obj.host}/hp/device/DeviceStatus/Index`, browser_params);
+async function get_device_details(printer_obj){
 
-    let cartridges = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".cartridges .consumable h2"))
-            .map (x=> x.textContent);
+    return new Promise((resolve, reject) => {
+
+        const page = system.browser.newPage();
+        page.goto(`https://${printer_obj.host}/hp/device/DeviceInformation/View`, browser_params);
+
+        printer_obj.model = page.evaluate(()=> {
+            return document.querySelector("#ProductName").textContent;
+        })
+        printer_obj.name = page.evaluate(()=> {
+            return document.querySelector("#DeviceName").textContent;
+        });
+        printer_obj.serial = page.evaluate(()=> {
+            return document.querySelector("#DeviceSerialNumber").textContent;
+        });
+        printer_obj.location = page.evaluate(()=> {
+            return document.querySelector("#DeviceLocation").textContent;
+        });
+
+        resolve("1");
+
     });
-
-    let levels = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".cartridges .consumable .plr"))
-            .map (x=> x.textContent.replace("%*", ''));
-    });
-
-    for(let i = 0; i < cartridges.length; ++i) {
-        printer_obj.supplies[cartridges[i]] = levels[i];
-    }
 
 }
 
-async function get_tray_details(browser, printer_obj){
-    const page = await browser.newPage();
-    await page.goto(`https://${printer_obj.host}/hp/device/DeviceStatus/Index`, browser_params);
+async function get_supply_details(printer_obj){
 
-    async function get_tray_info(tray_no) {
-        return {
-            status: (await page.evaluate((tray_no) => {
-                return document.querySelector(`#TrayBinStatus_${tray_no}`).textContent;
-            }, tray_no)).replace('%', ''),
-            capacity: await page.evaluate((tray_no) => {
-                return document.querySelector(`#TrayBinCapacity_${tray_no}`).textContent;
-            }, tray_no),
-            size: (await page.evaluate((tray_no) => {
-                return document.querySelector(`#TrayBinSize_${tray_no}`).textContent;
-            }, tray_no)).replace("▭", '').trim(),
-            type: await page.evaluate((tray_no) => {
-                return document.querySelector(`#TrayBinType_${tray_no}`).textContent;
-            }, tray_no)
-        };
-    }
-    async function tray_exists(tray_no) {
-        let div = await page.evaluate((tray_no) => {
-            return document.querySelector(`#TrayBin_Tray${tray_no}`);
-        }, tray_no);
-        return div != null;
-    }
+    return new Promise((resolve, reject) => {
 
-    if((await page.$("#TrayBin_MultipurposeTray"))) {
-        printer_obj.trays["Tray 1"] = await get_tray_info(1);
-    }
+        const page = system.browser.newPage();
+        page.goto(`https://${printer_obj.host}/hp/device/DeviceStatus/Index`, browser_params);
 
-    let tray = 2;
-    while(await tray_exists(tray)) {
-        printer_obj.trays[`Tray ${tray}`] = await get_tray_info(tray);
-        ++tray;
-    }
+        let cartridges = page.evaluate(() => {
+            return Array.from(document.querySelectorAll(".cartridges .consumable h2"))
+                .map (x=> x.textContent);
+        });
 
-    let machine_status_array = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll("#MachineStatus"))
-            .map(x=> x.textContent.trim())
+        let levels = page.evaluate(() => {
+            return Array.from(document.querySelectorAll(".cartridges .consumable .plr"))
+                .map (x=> x.textContent.replace("%*", ''));
+        });
+
+        for(let i = 0; i < cartridges.length; ++i) {
+            printer_obj.supplies[cartridges[i]] = levels[i];
+        }
+        resolve("1");
     });
 
-    printer_obj.errors = machine_status_array.filter(msg => msg !== "Ready");
+}
+
+async function get_tray_details(printer_obj){
+
+    return new Promise((resolve, reject) => {
+
+        const page = system.browser.newPage();
+        page.goto(`https://${printer_obj.host}/hp/device/DeviceStatus/Index`, browser_params);
+
+        async function get_tray_info(tray_no) {
+            return {
+                status: (await page.evaluate((tray_no) => {
+                    return document.querySelector(`#TrayBinStatus_${tray_no}`).textContent;
+                }, tray_no)).replace('%', ''),
+                capacity: await page.evaluate((tray_no) => {
+                    return document.querySelector(`#TrayBinCapacity_${tray_no}`).textContent;
+                }, tray_no),
+                size: (await page.evaluate((tray_no) => {
+                    return document.querySelector(`#TrayBinSize_${tray_no}`).textContent;
+                }, tray_no)).replace("▭", '').trim(),
+                type: await page.evaluate((tray_no) => {
+                    return document.querySelector(`#TrayBinType_${tray_no}`).textContent;
+                }, tray_no)
+            };
+        }
+        async function tray_exists(tray_no) {
+            let div = await page.evaluate((tray_no) => {
+                return document.querySelector(`#TrayBin_Tray${tray_no}`);
+            }, tray_no);
+            return div != null;
+        }
+
+        if((page.$("#TrayBin_MultipurposeTray"))) {
+            printer_obj.trays["Tray 1"] = get_tray_info(1);
+        }
+
+        let tray = 2;
+        while(tray_exists(tray)) {
+            printer_obj.trays[`Tray ${tray}`] = get_tray_info(tray);
+            ++tray;
+        }
+
+        let machine_status_array = page.evaluate(() => {
+            return Array.from(document.querySelectorAll("#MachineStatus"))
+                .map(x=> x.textContent.trim())
+        });
+
+        printer_obj.errors = machine_status_array.filter(msg => msg !== "Ready");
+        resolve("1");
+    });
 
 }
 
-async function handle_error(result){
-
-    let [device_details, supply_details, tray_details] = result;
-
-    if (device_details.status === "rejected")
-        return "Cannot get device details";
-    else if (supply_details.status === "rejected")
-        return "Cannot get supplies";
-    else if (tray_details.status === "rejected")
-        return "Cannot get Tray data";
-    else
-        return "Internal server error";
-}
+// async function handle_error(result){
+//
+//     return new Promise((resolve, reject) => {});
+//
+//     let [device_details, supply_details, tray_details] = result;
+//
+//     if (device_details.status === "rejected")
+//         return "Cannot get device details";
+//     else if (supply_details.status === "rejected")
+//         return "Cannot get supplies";
+//     else if (tray_details.status === "rejected")
+//         return "Cannot get Tray data";
+//     else
+//         return "Internal server error";
+// }
 
 
 class Printer {
-    constructor(host)
-    {
+    constructor(host) {
         this.host = host;
         this.name = null;
         this.type = null;
@@ -149,49 +182,36 @@ class Printer {
         this.errors = null;
     }
 
-    async get_info()
-    {
-        const browser = await pupp.launch({
-            headless: true,
-            devtools: false,
-            ignoreHTTPSErrors: true,
-            args: ['--incognito', '--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-sandbox']
-        });
-
+    async get_info() {
         // See if printer is online, if not return an error
-        if (!await isReachable(this.host))
-        {
-            await browser.close();
+        if (!await isReachable(this.host)) {
             printer_message = {
                 status: "error",
                 message: "IP address unreachable"
             };
-            await browser.close();
         }
 
-        else
-        {
-            const result = await Promise.allSettled([
-                get_device_details(browser, this),
-                get_supply_details(browser, this),
-                get_tray_details(browser, this),
+        else {
+            let [device_details,
+                supply_details,
+                tray_details]
+                = await Promise.allSettled([
+                get_device_details(this),
+                get_supply_details(this),
+                get_tray_details(this),
                 ]
             );
 
-            let [device_details,
-                supply_details,
-                tray_details] = result;
+            console.log(get_device_details(this));
 
-
-            if (device_details.status === "rejected" ||
-                supply_details.status === "rejected" ||
-                tray_details.status === "rejected" ) {
-
+            if (await device_details.status === "rejected" ||
+                await supply_details.status === "rejected" ||
+                await tray_details.status === "rejected"    )
+            {
                 printer_message = {
                     status: "error",
-                    message: handle_error(result)
+
                 };
-                await browser.close();
             }
 
             else {
@@ -201,20 +221,19 @@ class Printer {
                     message: this
                 };
                 request_message.ip = this.host;
-                await browser.close();
             }
         }
     }
 }
 
-
 app.get('/', async (req, res) =>
 {
     let ip = req.query.ip;
-    if (ip === undefined) {
+    if (ip === undefined)
         res.status(200).json(response_message);
-    }
-    else {
+
+    else
+    {
         if (isIPV4Address(ip.trim()))
         {
             let printer = new Printer(ip.trim());
@@ -231,6 +250,7 @@ app.get('/', async (req, res) =>
                 message: "Invalid IPV4 Address"
             }
             res.status(400).json(response_message);
+            await system.browser.close();
         }
     }
 
